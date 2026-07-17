375943f7ecaa1f21cecdb160dd59d43dc91b619c
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-07-17 11:29:33 +0200

    Updated config

--- init.lua
+++ init.lua
@@ -1,23 +1,54 @@
--- Example nnwm configuration
--- NOTE that this example configuration is not exhaustive in the sense that it doesn not include all the available options.
--- For a complete list of options, please refer to the documentation.
+-- nnwm configuration
 
 -- Layout
-nnwm.master_ratio = 0.55
-nnwm.master_ratio_step = 0.05
-nnwm.master_ratio_min = 0.1
-nnwm.master_ratio_max = 0.9
-nnwm.focus_follows_mouse = true
-nnwm.new_window_master = false
-nnwm.titlebars = true
-nnwm.titlebar_height = 20
-nnwm.titlebar_text_align = 0
-nnwm.titlebar_text_align = 2
+local hostname = nnwm.host_name()
+
+if hostname == "archlinux" then
+    nnwm.monitor({
+        description = "AU Optronics 0xE3AC Unknown",
+        x = 0,
+        y = 0,
+        width = 1920,
+        height = 1200,
+        scale = 1.25,
+        vrr = false,
+    })
+
+    nnwm.monitor({
+        description = "HP Inc. HP P24h G5 3CM5031JJC",
+        x = 1536,
+        y = 0,
+        width = 1920,
+        height = 1080,
+        scale = 1.0,
+    })
+
+    nnwm.monitor({
+        description = "HP Inc. HP 524pu 1H35321M53",
+        x = 3456,
+        y = 0,
+        width = 1920,
+        height = 1080,
+        scale = 1.0,
+    })
+elseif hostname == "matrix" then
+    nnwm.monitor({
+        name = "eDP-1",
+        scale = 1.25
+    })
+end
 
 nnwm.opt = {
 
+    -- monitors = {
+    --
+    -- }
+
     fx = {
-        corner_radius = 10,
+        rounding = {
+            radius = 10,
+            smart = true,
+        },
         shadow = {
             color = "#444444",
             blur_sigma = 10,
@@ -27,53 +58,48 @@ nnwm.opt = {
         },
     },
 
+    layout = {
+        master_ratio = 0.55,
+        master_ratio_step = 0.05,
+        master_ratio_min = 0.1,
+        master_ratio_max = 0.9,
+        focus_follows_mouse = true,
+        new_window_master = false,
+
+        tabbed = {
+            tab_position = "left",
+            tab_style = "minimal",
+            smart = true,
+        }
+    },
+
+    titlebar = {
+        enabled = true,
+        height = 30,
+        text_align = 0,
+    },
+
     keyboard = {
         xkb_options = "ctrl:swapcaps",
     },
 
     mouse = {
+        warp_to_focused_window = false,
         focus_follows_mouse = true,
+        hide_cursor_when_typing = false,
     },
 
     gaps = {
         inner = 20,
         outer = 20,
+        smart = false,
     },
 
     client_decorations = false,
 
     border = {
-
+        smart = false,
     },
-
-    monitors = {
-        {
-            description = "AU Optronics 0xE3AC Unknown",
-            x = 0,
-            y = 0,
-            width = 1920,
-            height = 1200,
-            scale = 1.25,
-        },
-        {
-            description = "HP Inc. HP P24h G5 3CM5031JJC",
-            x = 1536,
-            y = 0,
-            width = 1920,
-            height = 1080,
-            scale = 1.0,
-        },
-        {
-            description = "HP Inc. HP 524pu 1H35321M53",
-            x = 3456,
-            y = 0,
-            width = 1920,
-            height = 1080,
-            scale = 1.0,
-        },
-    },
-
-
 }
 
 local mod = "Super"
@@ -82,10 +108,19 @@ local mod = "Super"
 nnwm.key({ mod, "Shift", "c" }, function() nnwm.quit() end)
 nnwm.key({ mod, "Shift", "q" }, function() nnwm.close() end)
 nnwm.key({ mod, "p" }, function() nnwm.spawn("rofi -show drun") end)
-nnwm.key({ mod, "h" }, function() nnwm.master_ratio_shrink() end)
+-- nnwm.key({ mod, "h" }, function() nnwm.master_ratio_shrink() end)
+-- nnwm.key({ mod, "l" }, function() nnwm.master_ratio_grow() end)
+
+-- Logical directional focus
+nnwm.key({ mod, "h" }, function() nnwm.focus_dir("left") end)
+nnwm.key({ mod, "l" }, function() nnwm.focus_dir("right") end)
+
+-- Logical directional move
+nnwm.key({ mod, "Shift", "h" }, function() nnwm.move_dir("left") end)
+nnwm.key({ mod, "Shift", "l" }, function() nnwm.move_dir("right") end)
+
 nnwm.key({ mod, "j" }, function() nnwm.focus_next() end)
 nnwm.key({ mod, "k" }, function() nnwm.focus_prev() end)
-nnwm.key({ mod, "l" }, function() nnwm.master_ratio_grow() end)
 nnwm.key({ mod, "Shift", "h" }, function() nnwm.swap_left() end)
 nnwm.key({ mod, "Shift", "j" }, function() nnwm.swap_next() end)
 nnwm.key({ mod, "Shift", "k" }, function() nnwm.swap_prev() end)
@@ -115,7 +150,15 @@ for i = 1, 9 do
 end
 
 nnwm.spawn_once("waybar -c ~/.config/waybar/config.jsonc")
-nnwm.spawn_once("swaybg -i ~/Gits/wallpapers/1920x1080_px_black_geometry_gray_minimalism_Triangle_white-1286005.jpg -m fill")
+nnwm.spawn_once("swaybg -i ~/Gits/wallpapers/11.png -m fill")
 nnwm.spawn_once("dunst")
+nnwm.spawn_once("/usr/lib/xdg-desktop-portal -r")
+nnwm.spawn_once("/usr/lib/xdg-desktop-portal-wlr -r")
+nnwm.spawn_once("nm-applet")
+
+-- nnwm.gesture(3, "left",  function() nnwm.move_to_workspace(nnwm.active_workspace() - 1) end)
+-- nnwm.gesture(3, "right", function() nnwm.switch_workspace(nnwm.active_workspace() + 1) end)
+-- nnwm.gesture(4, "up",    function() nnwm.toggle_fullscreen() end)
+-- nnwm.gesture(4, "down",  function() nnwm.toggle_float() end)
 
 nnwm.rule({ app_id = "pcmanfm" }, { floating = true, titlebar = true })
